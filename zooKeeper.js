9b839748c868a9575fcf6ba0b8e96060b9b81928
Nergis Sadiklar <79223424+nergissadiklar@users.noreply.github.com>
2022-07-13 13:03:45 +0300

    Stage 2 was completed

--- zooKeeper.js
+++ zooKeeper.js
@@ -3,3 +3,25 @@ console.log("Let's check on the animals...");
 console.log("The deer looks fine.");
 console.log("The bat looks happy.");
 console.log("The lion looks healthy.");
+
+animal = `
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \\    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!`
+console.log(animal);
