b63a0dec6accdfbaf998b6f379dc67f069584635
Nergis Sadiklar <79223424+nergissadiklar@users.noreply.github.com>
2022-07-13 15:18:32 +0300

    Stage 3 was completed

--- zooKeeper.js
+++ zooKeeper.js
@@ -25,3 +25,8 @@ Switching on the camera in the camel habitat...
            /_I      L_I   L_I     /_I
 Look at that! Our little camel is sunbathing!`
 console.log(animal);
+answer = input("Which habitat # do you need?");
+animal= [camel,lion,deer,goose,bat,rabbit]
+console.log(animal[answer]);
+console.log(`---
+You've reached the end of the program. To check another habitat, please restart the watcher.`)
